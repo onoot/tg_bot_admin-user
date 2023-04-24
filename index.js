e48bf21c7d447950b4d6e56486b5827152a83ede
onoot <101798777+onoot@users.noreply.github.com>
2023-04-24 11:29:32 +0700

    1

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const TelegramBot = require('node-telegram-bot-api');
 const fs = require('fs');
-
 require('dotenv').config();
 
 const token = process.env.Telegram_bot_token;
 const admin = process.env.Admin;
+
 const bot = new TelegramBot(token, { polling: true });
 
 const commands = {
@@ -17,39 +17,83 @@ bot.setMyCommands([
     { command: '/info', description: commands.info },
 ]);
 
-    if (/\//) {
-        bot.onText(/\/info/, (msg) => {
-            bot.sendMessage(msg.chat.id, "Привет! Я информатор. Это информационное сообщение, которое должно дать вам полезную информацию.");
-        });
-    }
-    if (/\//){
-        bot.onText(/\//, (msg) => {
-            if (/help/i.test(msg.text)) {
-                bot.sendMessage(msg.chat.id, "Привет! Я помощник. Это сообщение помощи, которое должно помочь вам в использовании бота. Список доступных команд: " +
-                    " /help " +
-                    " /info");
-            }
-        });
-     if (/\//){
-            if (/\/start/) {
-                bot.onText(/\/start/, (msg) => {
-                   bot.sendMessage(msg.chat.id, "Здравствуйте! Вы очень важны для нас. Администраторы будут с вами связываться в ближайшее время.", {}).then(r => 0);
-                }) ;
-            }
-            else {
-                bot.sendMessage(msg.chat.id, "Введите /help, чтобы получить список доступных команд.");
-            }
-            }
-}
+// Обработка текстовых сообщений
+bot.on('text', (msg) => {
+    const chatId = msg.chat.id;
 
+    if (chatId == admin && msg.reply_to_message) {
+        // Если сообщение от админа и это ответ на сообщение
+        bot.sendMessage(msg.reply_to_message.forward_from.id, msg.text);
+    } else if (chatId != admin) {
+        // Если сообщение от обычного пользователя, пересылаем его админу
+        bot.forwardMessage(admin, chatId, msg.message_id);
+    }
+
+    if (msg.text.startsWith('/help')) {
+        // Обработка команды /help
+        bot.sendMessage(chatId, "Привет! Я помощник. Это сообщение помощи, которое должно помочь вам в использовании бота. Список доступных команд: " +
+            " /help " +
+            " /info");
+    }else if (msg.text.startsWith('/info')) {
+        // Обработка команды /info
+        bot.sendMessage(chatId, "Привет! Я информатор. Это информационное сообщение, которое должно дать вам полезную информацию.");
+
+    }else if (msg.text.startsWith('/start')) {
+        // Обработка команды /start
+        bot.sendMessage(chatId, "Здравствуйте! Вы очень важны для нас. Администраторы с вами свяжутся в ближайшее время.");
+
+    }
+});
+
+// Обработка голосовых, файловых и изображений сообщений
 bot.on('message', (msg) => {
     const chatId = msg.chat.id;
 
-    if (chatId == admin && msg.reply_to_message) { // если сообщение пришло от админа и это ответ на сообщение
-        bot.sendMessage(msg.reply_to_message.forward_from.id, msg.text);
+    if (chatId == admin && msg.reply_to_message) {
+        // Если сообщение от админа и это ответ на сообщение
+        switch (msg.reply_to_message.forward_from.type) {
+            case 'voice':
+                if (msg.voice) {
+                    // Если это голосовое сообщение
+                    bot.sendVoice(msg.reply_to_message.forward_from.id, msg.voice.file_id);
+                } else {
+                    bot.sendMessage(admin, 'Не удалось обработать голосовое сообщение');
+                }
+                break;
+            case 'photo':
+                if (msg.photo && msg.photo.length) {
+                    // Если это сообщение с изображением
+                    const photo = msg.photo[msg.photo.length - 1];
+                    bot.sendPhoto(msg.reply_to_message.forward_from.id, photo.file_id);
+                } else {
+                    bot.sendMessage(admin, 'Не удалось обработать сообщение с изображением');
+                }
+                break;
+            case 'document':
+                if (msg.document) {
+                    // Если это сообщение с файлом
+                    bot.sendDocument(msg.reply_to_message.forward_from.id, msg.document.file_id);
+                } else {
+                    bot.sendMessage(admin, 'Не удалось обработать сообщение с файлом');
+                }
+                break;
+        }
     } else if (chatId != admin) {
-        // пересылаем сообщение админу
-        bot.forwardMessage(admin, chatId, msg.message_id);
+        // Если сообщение от обычного пользователя, пересылаем его админу
+        if (msg.voice) {
+            // Если это голосовое сообщение
+            bot.forwardMessage(admin, chatId, msg.message_id);
+        } else if (msg.photo && msg.photo.length) {
+            // Если это сообщение с изображением
+            const photo = msg.photo[msg.photo.length - 1];
+            bot.sendPhoto(admin, photo.file_id);
+        } else if (msg.document) {
+            // Если это сообщение с файлом
+            bot.sendDocument(admin, msg.document.file_id);
+        } else {
+            // Если это обычное текстовое сообщение, пересылаем его админу
+            bot.forwardMessage(admin, chatId, msg.message_id);
+        }
     }
 });
 
